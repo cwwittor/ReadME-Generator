d48761bbbfad4403823c67cb41c391cb7f009d8f
Christian Wittorp <cwwittor@ncsu.edu>
2021-02-22 13:14:29 -0500

    Added prompts

--- Develop/index.js
+++ Develop/index.js
@@ -5,45 +5,51 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
 inquirer.prompt([
     {
-        type: ,
-        message: ,
-        name:
+        type: "input",
+        message: "What is the title of the project?",
+        name: "title"
     },
     {
-        type: ,
-        message: ,
-        name:
+        type: "input",
+        message: "What does the project do?",
+        name: "description"
     },
     {
-        type: ,
-        message: ,
-        name:
+        type: "input",
+        message: "How is the project installed?",
+        name: "installation"
     },
     {
-        type: ,
-        message: ,
-        name:
+        type: "input",
+        message: "What is your github username?",
+        name: "github"
     },
     {
-        type: ,
-        message: ,
-        name:
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
     },
     {
-        type: ,
-        message: ,
-        name:
+        type: "input",
+        message: "How would I use the application?",
+        name: "usage"
     },
     {
-        type: ,
-        message: ,
-        name:
+        type: "input",
+        message: "What did you learn or find interesting with the project?",
+        name: "interesting"
     },
     {
-        type: ,
-        message: ,
-        name:
+        type: "input",
+        message: "How would you test/run this application?",
+        name: "test"
     },
+    {
+        type: "list",
+        message: "What license do you need for this project?",
+        name: "license",
+        choices: ["Apache", "GNU", "MIT", "none"]
+    }
 ])
 
 
